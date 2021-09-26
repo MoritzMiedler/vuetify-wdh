bab71a3159ae0b1cbde5b91cab089926150c9ca7
Moritz Miedler <71124607+MoritzMiedler@users.noreply.github.com>
2021-09-26 11:05:54 +0200

    bruh4

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app>
+    <LogoBar></LogoBar>
     <v-main>
       <CarCards></CarCards>
     </v-main>
@@ -8,11 +9,13 @@
 
 <script>
 import CarCards from '@/views/CarCards.vue';
+import LogoBar from '@/views/LogoBar.vue';
 export default {
   name: 'App',
   data: () => ({}),
   components: {
     CarCards,
+    LogoBar,
   },
 };
 </script>
--- client/src/views/LogoBar.vue
+++ client/src/views/LogoBar.vue
@@ -1,3 +1,11 @@
 <template>
-    
+  <v-app-bar class="black" height="150px">
+    <img class="mx-2" src="/logo.png" style="height:100px; margin-top:0px" />
+  </v-app-bar>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
