7b068071268a2e83dd0b8de6d2aef65331c13b3c
Moritz Miedler <71124607+MoritzMiedler@users.noreply.github.com>
2021-09-26 10:48:48 +0200

    bruh3

--- client/src/views/CarCard.vue
+++ client/src/views/CarCard.vue
@@ -1,18 +1,21 @@
 <template>
   <v-card>
     <v-img :src="cardata.image"></v-img>
-    <span class="text-h5">{{ cardata.title }}</span>
-    <br />
-    <div>
-      {{ cardata.owner.firstName }} {{ cardata.owner.lastName }}
+
+    <div class="pa-3">
+      <span class="text-h5">{{ cardata.title }}</span>
       <br />
-      {{ cardata.yearOfMake }}
+      Owner: <span class="font-weight-bold">{{ cardata.owner.firstName }} {{ cardata.owner.lastName }}</span>
       <br />
-      {{ cardata.miles }}
+      Year: <span class="font-weight-bold">{{ cardata.yearOfMake }}</span>
       <br />
-      {{ cardata.price }}
+      Miles: <span class="font-weight-bold">{{ cardata.miles }} </span>
+      <br />
+      Price: <span class="font-weight-bold"> {{ cardata.price }} </span>
       <br />
       {{ cardata.description }}
+      <br />
+      <v-btn class="red darken-3 mt-2 white--text">Buy Car</v-btn>
     </div>
   </v-card>
 </template>
